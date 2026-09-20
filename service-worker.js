7c5edcb0a7dc3302c677a898cac0808faa705f18
evacuatorkrasnodar-ux <evacuatorkrasnodar@gmail.com>
2026-09-20 17:57:55 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,21 @@
-const CACHE = "evakuator-premium-v1";
-
-const ASSETS = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/app.js",
-  "/banner-top.png",
-  "/banner-top-mobile.png",
-  "/favicon.png",
-  "/manifest.json"
+const CACHE_NAME = 'evakuator-cache-v1';
+const URLS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/prices.html',
+  '/en.html'
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS))
   );
-  self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => key !== CACHE ? caches.delete(key) : null)
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request).then(response => {
-        const copy = response.clone();
-        caches.open(CACHE).then(cache => cache.put(event.request, copy));
-        return response;
-      });
-    })
+    caches.match(event.request).then((resp) => resp || fetch(event.request))
   );
 });
